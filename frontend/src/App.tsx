e3fe87fd795fd3b98c19095c3f154c08d48ae7f7
Arin0303 <ape4246@gmail.com>
2026-06-30 02:22:28 +0900

    fix: add page router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import StartPage from "./pages/StartPage/startPage";
+import LoginPage from "./pages/LoginPage/loginPage";
 
 function App() {
   return (
-    <div className="app">
-      <StartPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/guardian/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
